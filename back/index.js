f5e0d67e24a3efa5e749e4303b99ca7c83b408f6
LuckyLaszlo <luke.perrey.dev@gmail.com>
2017-12-15 19:11:43 +0100

    starting using mongoose

--- back/index.js
+++ back/index.js
@@ -1,12 +1,9 @@
 
 var express = require('express');
 var bodyParser = require('body-parser');
-
-var _tokens = [];
 var MongoClient = require('mongodb').MongoClient;
-var url = 'mongodb://localhost:27017';
-var _client = "";
-
+var url = 'mongodb://localhost:27017/mountain_shop';
+var mongoose = require('mongoose');
 var app = express();
 
 app.use(function(req, res, next) {
@@ -19,42 +16,46 @@ app.use(function(req, res, next) {
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 
+mongoose.connect(url, { useMongoClient: true, }, function(err) {
+  if (err) { throw err; } else {
+  console.log("Connected successfully to server with Mongoose");
+  console.log("----------------");
+  // console.log(mongoose.connection); 
+}
 
+// Deprecated method
+/* mongoose.connect(url, function(err) {
+   if (err) { throw err; } else {
+   console.log("Connected successfully to server with Mongoose");
+   console.log("----------------");
+   // console.log(mongoose.connection); 
+ } */
 
-MongoClient.connect(url, function(err, client) {
-  console.log("Connected successfully to server");
-  _client = client;
 });
 
-
 function _findUser(username) {
- var db = _client.db('simplon');
- db.collection('users').find({}).toArray(function(err, docs){
+  var db = _client.db('mountain_shop');
+ db.collection('customers').find({}).toArray(function(err, docs){
    return docs.username == username;
  });
 };
 
 
-app.get('/users', function(req, res) {
-  var token = '';
+app.get('/customers', function(req, res) {
 
-  if (req.query.accesstoken) token = req.query.accesstoken;
-  if (req.headers.accesstoken) token = req.headers.accesstoken;
+  var db = _client.db('mountain_shop');
 
-  var index = _tokens.indexOf(token);
-  var db = _client.db('simplon');
-
-  db.collection('users').find({}).toArray(function(err, docs) {
+  db.collection('customers').find({}).toArray(function(err, docs) {
         res.status(200).send(docs);
   });
   
 });
 
-app.get('/users/:id', function(req, res) {
+app.get('/customers/:id', function(req, res) {
   var id = req.params.id;
-  var db = _client.db('simplon');
+  var db = _client.db('mountain_shop');
 
-  db.collection('users').find({username: id}).toArray(function(err, docs) {
+  db.collection('customers').find({username: id}).toArray(function(err, docs) {
         res.status(200).send(docs);
   });
 });
@@ -63,17 +64,13 @@ app.post('/login', function(req, res) {
   var body = req.body;
 
   if (body.username && body.password) {
-    var db = _client.db('simplon');
-    db.collection('users').find({username : body.username}).toArray(function(err, docs) {
+    var db = _client.db('mountain_shop');
+    db.collection('customers').find({username : body.username}).toArray(function(err, docs) {
         if (docs) {
           doc = docs[0];
           console.log(doc.password, body.password)
           if (doc.password == body.password) {
-            var token = doc.username + Math.floor(Math.random() * 100000) + 1;
-            console.log("test");
-            _tokens.push(token);
-
-            res.status(200).send({message: 'Login correct', token: token});
+            res.status(200).send({message: 'Login correct'});
           } 
           else {
             res.status(412).send('Password does not match');
@@ -94,7 +91,7 @@ app.post('/login', function(req, res) {
 
 app.post('/create-account', function(req, res) {
  var body = req.body;
- var db = _client.db('simplon');
+  var db = _client.db('mountain_shop');
  if (body.username && body.password) {
    if (_findUser(body.username)) {
      res.status(409).send('User already exists with username: ' + body.username);
@@ -106,7 +103,7 @@ app.post('/create-account', function(req, res) {
        lastName: body.lastName,
        age: body.age,
      };
-     db.collection('users').save(newProfile);
+     db.collection('customers').save(newProfile);
 
      res.status(200).send('User created with success :visage_légèrement_souriant: !');
   }
@@ -115,6 +112,28 @@ else{
   res.status(412).send('You should provide all the required fields: username, password');
 }
 });
+
+// -----------------------------------------------------
+// ----------------------Schema-------------------------
+
+var productsSchema = new mongoose.Schema({
+  ref: Number,
+  type: String,
+  name: String,
+  brand: String,
+  price: Number,
+  message: String
+  // osef : { type: String, match: /^[a-zA-Z0-9-_]+$/ },
+});
+
+var customersSchema = new mongoose.Schema({
+  // email send by the front must be case insensitive :)
+  email: String,
+  password: String,
+  creationDate: { type: Date, default: Date.now }
+});
+
+
 
 app.listen(3000, function() {
   console.log('BACKEND LISTENING ON PORT 3000');
